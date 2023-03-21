5586fec1539c6bc0cc428c067046fde3eca553b8
Jeff Serota <jeff.serota@gmail.com>
2023-03-21 12:49:51 -0400

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let endString = ''
+    if (typeof(num) == "number" && num >= 0){ 
+    for (let i = 0; i < num; i++){
+        endString +=  string;
+    };
+    return endString;
+} else {
+    return "ERROR";
+}
 };
 
 // Do not edit below this line
