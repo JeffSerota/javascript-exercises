f001da78e655fc488905fe0b2b2fbbe77f009b4e
Jeff Serota <jeff.serota@gmail.com>
2023-03-24 17:09:54 -0400

    Compelted exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+        let today = new Date();
+        let oldestPerson = people[0];
+      
+        people.forEach(person => {
+          let yearOfDeath = person.yearOfDeath || today.getFullYear();
+          let age = yearOfDeath - person.yearOfBirth;
+          let oldestAge = oldestPerson.yearOfDeath ? oldestPerson.yearOfDeath - oldestPerson.yearOfBirth : today.getFullYear() - oldestPerson.yearOfBirth;
+      
+          if (age > oldestAge) {
+            oldestPerson = person;
+          }
+        });
+      
+        return oldestPerson;
+      }
+      
 
 // Do not edit below this line
 module.exports = findTheOldest;
