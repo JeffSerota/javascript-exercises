d7dee78eac9bfeeab2fbff95353e9a6e9ee852af
Jeff Serota <jeff.serota@gmail.com>
2023-03-24 16:44:22 -0400

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    str = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  
+    // Check if the string is equal to its reverse
+    return str === str.split('').reverse().join('');
+  }
 
 // Do not edit below this line
 module.exports = palindromes;
