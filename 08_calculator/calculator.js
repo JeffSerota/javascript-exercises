1c5226dfdd512d40a47e4878e631bccf57092b85
Jeff Serota <jeff.serota@gmail.com>
2023-03-24 16:42:07 -0400

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (Number.isInteger(arr[i])) {
+      sum += arr[i];
+    }
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+      result *= arr[i];
+    }
+  }
+  return result;
+}
 
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+    if (n === 1 || n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
 
 // Do not edit below this line
 module.exports = {
