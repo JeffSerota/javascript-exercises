57afddc7909a6984e09d3d4edde0c52276432ea9
Jeff Serota <jeff.serota@gmail.com>
2023-03-24 16:56:10 -0400

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) return "OOPS"; // reject negative numbers
 
-};
+    n = parseInt(n, 10); // parse the input as an integer
+
+    let a = 0;
+    let b = 1;
+
+    if (n === 0) {
+        return a;
+    } else if (n === 1) {
+        return b;
+    } else {
+        for (let i = 2; i <= n; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
